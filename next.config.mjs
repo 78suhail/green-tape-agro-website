de4166c42883c9f1668bcc4cbe1a0cce83dbfc92
78suhail <suhail.codinghub@gmail.com>
2026-02-14 19:16:52 +0530

    Prepare project for clean push

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   typescript: {
     ignoreBuildErrors: true,
   }
 }
 
 export default nextConfig
-=======
-  /* config options here */
-  reactCompiler: true,
-};
-
-export default nextConfig;
->>>>>>> 8814d81657710591d9469425c06200a13117222a
